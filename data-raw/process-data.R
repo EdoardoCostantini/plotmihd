fc03316280e5319379bf3efc38a801ff6afa596c
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-05-02 17:19:22 +0200

    feat: add collinearity results data

---
+++ data-raw/process-data.R
@@ -0,0 +1,26 @@
+# Project:   mihd.results
+# Objective: Prepare and deploy results for app
+# Author:    Edoardo Costantini
+# Created:   2023-05-02
+# Modified:  2023-05-02
+# Notes: 
+
+library(stringr)
+
+# Load data --------------------------------------------------------------------
+
+# Load results for simulation study 1.2
+res_exp_1_2 <- readRDS("./data-raw/exp1_2_simOut_20230425_0918_res.rds")
+
+# Simulation study 1.2: results ------------------------------------------------
+
+# Change names of methods if required
+levels(res_exp_1_2$methods) <- str_replace(levels(res_exp_1_2$methods), "blasso", "BLasso")
+levels(res_exp_1_2$methods) <- str_replace(levels(res_exp_1_2$methods), "bridge", "BRidge")
+levels(res_exp_1_2$methods) <- str_replace(levels(res_exp_1_2$methods), "MI-qp", "MI-QP")
+levels(res_exp_1_2$methods) <- str_replace(levels(res_exp_1_2$methods), "MI-am", "MI-AM")
+levels(res_exp_1_2$methods) <- str_replace(levels(res_exp_1_2$methods), "MI-OP", "MI-OR")
+levels(res_exp_1_2$methods) <- str_replace(levels(res_exp_1_2$methods), "stepFor", "MI-SF")
+
+# Use the data
+usethis::use_data(res_exp_1_2, overwrite = TRUE)

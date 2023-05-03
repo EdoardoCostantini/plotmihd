5f8cebdbc950783bf9d68fd1dd0185e5ff06bbec
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-05-03 15:44:29 +0200

    content: add rds for time module 1 and module 2

--- R/data.R
+++ R/data.R
@@ -9,6 +9,17 @@
 #' @format A data frame
 NULL
 
+#' res_exp_1_time
+#'
+#' The data.frame containing the time results for the main simulation study.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name res_exp_1_time
+#' @usage data(res_exp_1_time)
+#' @format A data frame
+NULL
+
 #' res_exp_1_2
 #'
 #' The data.frame containing the results for simulation study 1.2 on collinearity.
@@ -17,5 +28,16 @@ NULL
 #' @keywords datasets
 #' @name res_exp_1_2
 #' @usage data(res_exp_1_2)
+#' @format A data frame
+NULL
+
+#' res_exp_1_2_time
+#'
+#' The data.frame containing the time results for simulation study 1.2 on collinearity.
+#'
+#' @docType data
+#' @keywords datasets
+#' @name res_exp_1_2_time
+#' @usage data(res_exp_1_2_time)
 #' @format A data frame
 NULL
--- data-raw/process-data.R
+++ data-raw/process-data.R
@@ -12,8 +12,14 @@ library(stringr)
 # Load experiment 1 results
 res_exp_1 <- readRDS("./data-raw/exp1_simOut_20230403_1631_res.rds")
 
+# Load simulation study 1 time
+res_exp_1_time <- readRDS("./data-raw/exp1_simOut_time.rds")
+
 # Load results for simulation study 1.2
 res_exp_1_2 <- readRDS("./data-raw/exp1_2_simOut_20230503_0838_res.rds")
+
+# Load simulation study 1.2 time
+res_exp_1_2_time <- readRDS("./data-raw/exp1_2_simOut_time.rds")
 
 # Simulation study: results ----------------------------------------------------
 
@@ -40,6 +46,33 @@ res_exp_1$analysis <- factor(
 
 # Use the data
 usethis::use_data(res_exp_1, overwrite = TRUE)
+
+# Simulation study: time -------------------------------------------------------
+
+# Change names of methods
+levels(res_exp_1$methods) <- str_replace(levels(res_exp_1$methods), "-la", "")
+levels(res_exp_1$methods) <- str_replace(levels(res_exp_1$methods), "blasso", "BLasso")
+levels(res_exp_1$methods) <- str_replace(levels(res_exp_1$methods), "bridge", "BRidge")
+levels(res_exp_1$methods) <- str_replace(levels(res_exp_1$methods), "MI-qp", "MI-QP")
+levels(res_exp_1$methods) <- str_replace(levels(res_exp_1$methods), "MI-am", "MI-AM")
+levels(res_exp_1$methods) <- str_replace(levels(res_exp_1$methods), "MI-OP", "MI-OR")
+levels(res_exp_1$methods) <- str_replace(levels(res_exp_1$methods), "stepFor", "MI-SF")
+
+# Fix methods order
+res_exp_1$methods <- factor(res_exp_1$methods,
+    levels = levels(res_exp_1$methods)[c(1:7, 13, 11:12, 8, 9, 10)]
+)
+
+# Make analysis a factor
+res_exp_1$analysis <- factor(
+    x = res_exp_1$analysis,
+    levels = unique(res_exp_1$analysis),
+    labels = c("PRB", "CIC", "CIW")
+)
+
+# Use the data
+usethis::use_data(res_exp_1, overwrite = TRUE)
+
 
 # Simulation study 1.2: results ------------------------------------------------
 

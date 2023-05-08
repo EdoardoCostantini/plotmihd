5886c1b907bbf2eb08d39b4ecb066da5a988b8b1
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-05-08 14:31:50 +0200

    content: improve phrasing of the MI-PCA text

--- R/tab_mi_pca_text.R
+++ R/tab_mi_pca_text.R
@@ -32,10 +32,10 @@ tab_mi_pca_text <- function(output) {
                     <li><b>Block 3</b>: variables v11 to v50 were uncorrelated among themselves and with the variables in blocks 2 and 3,</li>
                 </ul>
                 In the original simulation study, missing values were imposed on items v1 to v3 and v5 to v7 based on variables v4, v5, v9, and v10 (MAR predictors).
-                <b>MI-PCA</b> defined the <b>imputation model</b> for each of the variables with missing values to include as predictors:
+                For each of the variables with missing values, <b>MI-PCA</b> included the following variables as predictors in the <b>imputation model</b>:
                 <ul>
                     <li> The other variables with missing values (same as MI-AM.)</li>
-                    <li> The first principal components explaining at least 50% of the variance in the set of possible auxiliary predictors (composed of the MAR predictors and the variables in block 3, namely v4, v5, v9, v10, and v11 to v50.)</li>
+                    <li> The first principal components explaining at least 50% of the variance in the set of potential auxiliary predictors (composed of the MAR predictors and the variables in block 3, namely v4, v5, v9, v10, and v11 to v50.)</li>
                 </ul>
                 <br>
                 <br>"
@@ -54,13 +54,13 @@ tab_mi_pca_text <- function(output) {
                 The <b>color gradient</b> represents the strength of the bivariate associations, with darker colors representing higher correlation values.
                 <br>
                 <br>
-                As you change the values of the <code>Collinearity</code> input, you will notice changes in the correlation between variables v4 and v5, v9 and v10, and variables v11 to v15.
+                As you change the values of the <code>Collinearity</code> input, you will notice changes in the correlation between variables v4 and v5, v9 and v10, and variables v11 to v50.
                 <br>
                 Note the following:
                 <ul>
-                    <li>The correlation between variables v1 to v10 and the variables v11 to v50 is fixed to approximately 0, irrespective of the value of the input <code>Collinearity</code>.
-                        This <b>preserved</b> the understanding of variables v11 to v50 as <b>noise variables</b> in the task of imputing variables v1 to v10.
-                    <li>Expect for variables v4, v5, v9, and v10 The correlation between variables v1 to v10 also stays the same. This preserved the same strength of the MAR mechanism produced in the original simulation study. Because v4, v5, v9, and v10 are the MAR predictors used to impose missing values on v1 to v3 and v5 to v7, these correlations needed to stay constant, otherwise, the <b>strength of the MAR mechanism</b> would have changed together with the collinearity.</li>
+                    <li>The correlation between the block of variables v1 to v10 and the block of variables v11 to v50 is fixed to approximately 0, irrespective of the value of the input <code>Collinearity</code>.
+                        This <b>preserved</b> the understanding of variables v11 to v50 as <b>noise variables</b> in the task of imputing any variable in the block v1 to v10.
+                    <li>Expect for variables v4, v5, v9, and v10, the correlation between variables v1 to v10 remained constant. Because v4, v5, v9, and v10 are the MAR predictors used to generate missing values on v1 to v3 and v5 to v7, these correlations needed to stay constant to preserve the same strength of the MAR mechanism produced in the original simulation study. Had we changed the correlation between the MAR predictors and the variables with missing values, we would have changed the <b>strength of the MAR mechanism</b> together with the collinearity. This would have then made it impossible to attribute any change in the performance of the methods to either the different degrees of collinearity or the different strength of MAR.</li>
                 </ul>
                 <br>
                 <br>"
@@ -71,14 +71,14 @@ tab_mi_pca_text <- function(output) {
     output$heatmap_load_int <- renderText({
         shiny::HTML(
             "<br>
-                 The <b>heatmap in Panel B</b> shows the <b>principal component loadings</b> for the first 10 principal components (PCs).
+                 The <b>heatmap in Panel B</b> shows the absolute value of the <b>principal component loadings</b> for the first 10 principal components (PCs).
                  The color gradient represents the weight of a variable in the linear combination defining the PCs.
                  Darker colors indicate higher loadings (or weights) compared to other items.
                  Although no loading is ever exactly equal to 0, white and light gray indicate that a variable is only a weak contribution to the computation of a given PC.
                  <br>
                  <br>
-                 When <code>Collinearity</code> is set to 0, the PC loadings for v4, v5, v9, and v10 are many orders of magnitudes larger than the ones for all other variables.
-                 This configuration can be understood as generating a first principal component that is a good representation of the <b>MAR predictors</b>.
+                 When <code>Collinearity</code> is set to 0, the first PC loadings for v4, v5, v9, and v10 are many orders of magnitudes larger than the ones for all other variables.
+                 This configuration can be understood as generating a first PC that is a good representation of the <b>MAR predictors</b>.
                  As you increase the <code>Collinearity</code> input value, you will notice that the first PC becomes a linear combination of the <b>noise variables</b>, while v4, v5, v9, and v10 become important only in the computation of the second and third PCs.
                  <br>
                  <br>"
@@ -89,8 +89,8 @@ tab_mi_pca_text <- function(output) {
     output$hist_int <- renderText({
         shiny::HTML(
             "<br>
-                The <b>histogram in Panel C</b> shows the number of PCs kept when performing PCA on the potential auxiliary variables.
-                Five <b>non-graphical decision rules</b> are reported:
+                The <b>histogram in Panel C</b> shows the number of PCs kept when performing PCA on the potential auxiliary variables (v4, v5, v9, v10, and v11 to v50).
+                Five <b>non-graphical decision rules</b> were considered:
                 <ul>
                     <li>Optimal coordinates index (noc)</li>
                     <li>Acceleration factor (naf)</li>
@@ -98,7 +98,7 @@ tab_mi_pca_text <- function(output) {
                     <li>Kaiser criterion (nkaiser)</li>
                     <li>50% rule (rule50) - This is the rule used in <a href='https://arxiv.org/abs/2208.13656'>Costantini et. al. (2022)</a></li>
                 </ul>
-                When <code>Collinearity</code> is set to 0, the number of components selected by the <b>50% rule</b> used in this study is quite high (around 19).
+                When <code>Collinearity</code> is set to 0, the number of components selected by the <b>50% rule</b> is quite high (around 19).
                 As you increase the value of <code>Collinearity</code>, the number selected by this rule decreases and reaches 1 for correlation values greater than 0.5.
                 For the same values, the <b>Kaiser rule</b> and the <b>parallel analysis</b> always identify 2 or more PCs.
                 <br>
@@ -131,12 +131,12 @@ tab_mi_pca_text <- function(output) {
                 As a result, the MAR predictors were present in the imputation model through the first PC, leading to the good imputation performance seen in the original study.
                 <br>
                 <br>
-                For a <code>Collinearity</code> of .9, the 50% rule retains a single PC that explains more than 70% of the variance in X.
+                For a <code>Collinearity</code> of .9, the 50% rule retains a single PC that explains more than 80% of the variance in the potential auxiliary variables.
                 However, this single component is a linear combination where the <b>noise variables</b> are weighted much more than the MAR predictors.
-                As a result, in <a href='https://arxiv.org/abs/2208.13656'>Costantini et. al. (2022a)</a>, the only additional predictor MI-PCA used compared to MI-AM was a linear combination of noise variables.
+                As a result, in this situation, the only additional predictor MI-PCA would use, compared to MI-AM, is a linear combination of noise variables.
                 <br>
                 <br>
-                In <a href='https://arxiv.org/abs/2208.13656'>Costantini et. al. (2022a)</a>, MI-PCA performed similarly to MI-AM in conditions with high correlations among the noise variables because the only additional predictor was a linear combination of noise variables.
+                In <a href='https://arxiv.org/abs/2208.13656'>Costantini et. al. (2022a)</a>, MI-PCA performed similarly to MI-AM in conditions with high correlations among the potential auxiliary variables because the only additional predictor was a linear combination of noise variables.
                 However, this does not suggest MI-PCA is inadequate for highly collinear data.
                 PCA does what it should: higher correlation values in the block of noise variables make this block the largest variation to explain, and the first PC will summarise it well.
                 The problem for MI-PCA was that the first component extracted had little relevance for the imputation task.

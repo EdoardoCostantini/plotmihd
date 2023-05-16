4a7e1d531648c879e0529de4adf00411041d4420
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2023-05-16 11:36:51 +0200

    format: change v to z to reference in module 3

--- R/tab_mi_pca_text.R
+++ R/tab_mi_pca_text.R
@@ -27,15 +27,15 @@ tab_mi_pca_text <- function(output) {
                 This Shiny app generates data according to the same data-generating procedure defined in Costantini et. al. (2022).
                 The original study sampled 200 observations from a multivariate normal distribution using a known covariance matrix with a 3-block structure:
                 <ul>
-                    <li><b>Block 1</b>: variables v1 to v5 were correlated by 0.6 among themselves.</li>
-                    <li><b>Block 2</b>: variables v6 to v10 were correlated by 0.3 among themselves and with the variables in block 1.</li>
-                    <li><b>Block 3</b>: variables v11 to v50 were uncorrelated among themselves and with the variables in blocks 2 and 3,</li>
+                    <li><b>Block 1</b>: variables z1 to z5 were correlated by 0.6 among themselves.</li>
+                    <li><b>Block 2</b>: variables z6 to z10 were correlated by 0.3 among themselves and with the variables in block 1.</li>
+                    <li><b>Block 3</b>: variables z11 to z50 were uncorrelated among themselves and with the variables in blocks 2 and 3,</li>
                 </ul>
-                In the original simulation study, missing values were imposed on items v1 to v3 and v5 to v7 based on variables v4, v5, v9, and v10 (MAR predictors).
+                In the original simulation study, missing values were imposed on items z1 to z3 and z5 to z7 based on variables z4, z5, z9, and z10 (MAR predictors).
                 For each of the variables with missing values, <b>MI-PCA</b> included the following variables as predictors in the <b>imputation model</b>:
                 <ul>
                     <li> The other variables with missing values (same as MI-AM.)</li>
-                    <li> The first principal components explaining at least 50% of the variance in the set of potential auxiliary predictors (composed of the MAR predictors and the variables in block 3, namely v4, v5, v9, v10, and v11 to v50.)</li>
+                    <li> The first principal components explaining at least 50% of the variance in the set of potential auxiliary predictors (composed of the MAR predictors and the variables in block 3, namely z4, z5, z9, z10, and z11 to z50.)</li>
                 </ul>
                 <br>
                 <br>"
@@ -47,20 +47,20 @@ tab_mi_pca_text <- function(output) {
         shiny::HTML(
             "<br>
                 During the review process, one referee highlighted the need to explore how the strategies compared in the original study performed for varying degrees of collinearity.
-                We introduced collinearity as an experimental factor in the simulation study by allowing the correlation between certain variables to vary.
+                We introduced collinearity as an experimental factor in the simulation study by allowing the correlation between certain variables to zary.
                 <br>
                 <br>
                 The <b>heatmap in Panel A</b> shows the correlation matrix of the data for different levels of the collinearity factor.
                 The <b>color gradient</b> represents the strength of the bivariate associations, with darker colors representing higher correlation values.
                 <br>
                 <br>
-                As you change the values of the <code>Collinearity</code> input, you will notice changes in the correlation between variables v4 and v5, v9 and v10, and variables v11 to v50.
+                As you change the values of the <code>Collinearity</code> input, you will notice changes in the correlation between variables z4 and z5, z9 and z10, and variables z11 to z50.
                 <br>
                 Note the following:
                 <ul>
-                    <li>The correlation between the block of variables v1 to v10 and the block of variables v11 to v50 is fixed to approximately 0, irrespective of the value of the input <code>Collinearity</code>.
-                        This <b>preserved</b> the understanding of variables v11 to v50 as <b>noise variables</b> in the task of imputing any variable in the block v1 to v10.
-                    <li>Expect for variables v4, v5, v9, and v10, the correlation between variables v1 to v10 remained constant. Because v4, v5, v9, and v10 are the MAR predictors used to generate missing values on v1 to v3 and v5 to v7, these correlations needed to stay constant to preserve the same strength of the MAR mechanism produced in the original simulation study. Had we changed the correlation between the MAR predictors and the variables with missing values, we would have changed the <b>strength of the MAR mechanism</b> together with the collinearity. This would have then made it impossible to attribute any change in the performance of the methods to either the different degrees of collinearity or the different strength of MAR.</li>
+                    <li>The correlation between the block of variables z1 to z10 and the block of variables z11 to z50 is fixed to approximately 0, irrespective of the value of the input <code>Collinearity</code>.
+                        This <b>preserved</b> the understanding of variables z11 to z50 as <b>noise variables</b> in the task of imputing any variable in the block z1 to z10.
+                    <li>Expect for variables z4, z5, z9, and z10, the correlation between variables z1 to z10 remained constant. Because z4, z5, z9, and z10 are the MAR predictors used to generate missing values on z1 to z3 and z5 to z7, these correlations needed to stay constant to preserve the same strength of the MAR mechanism produced in the original simulation study. Had we changed the correlation between the MAR predictors and the variables with missing values, we would have changed the <b>strength of the MAR mechanism</b> together with the collinearity. This would have then made it impossible to attribute any change in the performance of the methods to either the different degrees of collinearity or the different strength of MAR.</li>
                 </ul>
                 <br>
                 <br>"
@@ -77,9 +77,9 @@ tab_mi_pca_text <- function(output) {
                  Although no loading is ever exactly equal to 0, white and light gray indicate that a variable is only a weak contribution to the computation of a given PC.
                  <br>
                  <br>
-                 When <code>Collinearity</code> is set to 0, the first PC loadings for v4, v5, v9, and v10 are many orders of magnitudes larger than the ones for all other variables.
+                 When <code>Collinearity</code> is set to 0, the first PC loadings for z4, z5, z9, and z10 are many orders of magnitudes larger than the ones for all other variables.
                  This configuration can be understood as generating a first PC that is a good representation of the <b>MAR predictors</b>.
-                 As you increase the <code>Collinearity</code> input value, you will notice that the first PC becomes a linear combination of the <b>noise variables</b>, while v4, v5, v9, and v10 become important only in the computation of the second and third PCs.
+                 As you increase the <code>Collinearity</code> input value, you will notice that the first PC becomes a linear combination of the <b>noise variables</b>, while z4, z5, z9, and z10 become important only in the computation of the second and third PCs.
                  <br>
                  <br>"
         )
@@ -89,7 +89,7 @@ tab_mi_pca_text <- function(output) {
     output$hist_int <- renderText({
         shiny::HTML(
             "<br>
-                The <b>histogram in Panel C</b> shows the number of PCs kept when performing PCA on the potential auxiliary variables (v4, v5, v9, v10, and v11 to v50).
+                The <b>histogram in Panel C</b> shows the number of PCs kept when performing PCA on the potential auxiliary variables (v4, z5, z9, z10, and z11 to z50).
                 Five <b>non-graphical decision rules</b> were considered:
                 <ul>
                     <li>Optimal coordinates index (noc)</li>
@@ -115,7 +115,7 @@ tab_mi_pca_text <- function(output) {
                 <br>
                 When <code>Collinearity</code> is set to 0, the <b>first PC</b> explains very little of the total variance in the variables considered (CPVE < 0.01).
                 For larger values of the <code>Collinearity</code> input, the first PC explains more and more of the total variance.
-                As the strength of the association between the variables from v11 to v50 increases, more of the total variance is explained by the one component representing these variables.
+                As the strength of the association between the variables from z11 to z50 increases, more of the total variance is explained by the one component representing these variables.
                 For the highest values of <code>Collinearity</code>, an <b>elbow shape</b> appears in Panel D: the first 3 PCs collectively explain the majority of the variance in Z, and additional PCs only add a trivial amount of explained variance.
                 <br>
                 <br>"
